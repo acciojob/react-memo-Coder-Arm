39fa9ff6ab8b8e03aa8419a5505dc4db3b31759e
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-08-21 21:01:55 +0530

    again

--- src/components/Memo.js
+++ src/components/Memo.js
@@ -13,7 +13,7 @@ const Memo = () => {
 
     return(<div>
       <input type='text' id='skill-input' value={skill} onChange={(e)=> setSkill(e.target.value)}/>
-      <button onClick={addSkill}>Add Skill</button>
+      <button id='skill-btn' onClick={addSkill}>Add Skill</button>
       {memo.map(item => {
         return (
             <ul>
