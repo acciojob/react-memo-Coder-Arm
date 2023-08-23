bd415cbc6f0b63ade96dd8d78a5e282c42c46d6a
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-08-23 11:50:28 +0530

    again

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 const Counter = () => {
    const [count,setCount] = useState(0);
 
-   return(<div>
+   return(<div id='calc'>
    Count: <button id='incr-cnt' onClick={()=> setCount(count+1)}>{count}</button>
    </div>)
 }
